af4228a2571bbfd52366515229c03e992ae3718e
Yana Sirina <yanasirina0@yandex.ru>
2024-01-23 16:24:43 +0500

    настройка celery

---
+++ service/celery_app.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+from celery import Celery
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+app = Celery('project')
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
+
+@app.task()
+def debug_task():
+    time.sleep(20)
+    print('Hello form debug_task')
